8b39ffabeafd86b49cb8a36ca7fbcd789b13366f
Dmitry Konychev <dalexeych@gmail.com>
2022-07-28 10:24:33 +0300

    feat: user error handling

--- controllers/users.js
+++ controllers/users.js
@@ -1,34 +1,51 @@
 const User = require('../models/user');
+const BAD_REQUEST_ERROR = 400;
+const NOT_FOUND_ERROR = 404;
+const DEFAULT_ERROR = 500;
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send(user))
-    .catch((err) => res.status(500).send(err.message));
+    .catch((err) => {
+      if (err.name === 'ValidationError') res.status(BAD_REQUEST_ERROR).send({ message: 'Bad request' });
+      res.status(DEFAULT_ERROR).send({ message: 'internal server error' });
+    });
 };
 
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send(users))
-    .catch((err) => res.status(500).send(err.message));
+    .catch(() => res.status(DEFAULT_ERROR).send({ message: 'internal server error' }));
 };
 
 module.exports.getUserById = (req, res) => {
   User.findOne({ _id: req.params.userId })
     .then((user) => res.send(user))
-    .catch((err) => res.status(500).send(err.message));
+    .catch((err) => {
+      if (err.name === 'CastError') res.status(NOT_FOUND_ERROR).send({ message: 'User not found' });
+      res.status(DEFAULT_ERROR).send(err.message);
+    });
 };
 
 module.exports.patchUser = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .then((user) => res.send(user))
-    .catch((err) => res.status(500).send(err.message));
+    .catch((err) => {
+      if (err.name === 'CastError') res.status(NOT_FOUND_ERROR).send({ message: 'User not found' });
+      if (err.name === 'ValidationError') res.status(BAD_REQUEST_ERROR).send({ message: 'Bad request' });
+      res.status(500).send({ message: 'Internal server error' });
+    });
 };
 
 module.exports.patchAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .then((user) => res.send(user))
-    .catch((err) => res.status(500).send(err.message));
+    .catch((err) => {
+      if (err.name === 'CastError') res.status(NOT_FOUND_ERROR).send({ message: 'User not found' });
+      if (err.name === 'ValidationError') res.status(BAD_REQUEST_ERROR).send({ message: 'Bad request' });
+      res.status(500).send({ message: 'Internal server error' });
+    });
 };
