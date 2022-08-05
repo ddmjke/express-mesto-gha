0a5c768107b1dcb74210e706ff8569344ce99851
Dmitry Konychev <dalexeych@gmail.com>
2022-08-05 16:32:28 +0300

    feat: user schema with hash first draft

--- models/user.js
+++ models/user.js
@@ -1,24 +1,62 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const SALT_WORK_FACTOR = 10;
+
+const validateEmail = (email) => {
+  const res = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  return res.test(email);
+};
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Жак-Ив Кусто',
   },
 
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Исследователь',
   },
 
   avatar: {
     type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: validateEmail,
+      message: 'Неправильный формат почты',
+    },
+  },
+
+  password: {
+    type: String,
     required: true,
   },
+});
+
+userSchema.pre('save', (next) => {
+  const user = this;
+  if (!user.isModified('password')) return next();
+
+  bcrypt.hash(user.password, SALT_WORK_FACTOR)
+    .then((hash) => {
+      user.password = hash;
+      return next();
+    })
+    .catch(() => {
+      throw new Error('hash error');
+    });
+  return next();
 });
 
 module.exports = mongoose.model('user', userSchema);
